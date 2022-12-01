fe17cc952e7ad61281e89e1762df4ef25ce5324e
Desmond Andersen <desmond.andersen@devz.ai>
2022-11-30 20:00:50 -0800

    Add new gatsby site

---
+++ gatsby-config.js
@@ -0,0 +1,13 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-config/
+ */
+
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  pathPrefix: "/gatsby-portfolio", // Github repo name
+  plugins: [],
+};
---
+++ public/component---src-pages-index-js-4f419f6ab0a237ca4b9b.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_hello_world=self.webpackChunkgatsby_starter_hello_world||[]).push([[678],{558:function(e,t,l){l.r(t),l.d(t,{default:function(){return n}});var r=l(294);function n(){return r.createElement("div",null,"Hello world!")}}}]);
+//# sourceMappingURL=component---src-pages-index-js-4f419f6ab0a237ca4b9b.js.map
